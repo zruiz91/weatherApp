933076c4206bbdc75b19973ee9e1d8f3bb480aa8
Zach Ruiz <powersbooth10@gmail.com>
2023-04-28 13:47:25 -0500

    added comments to the index.js file.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// importing both the express module to start a server and the https module to handle request to external APIs
 const express = require('express');
 const https = require('https');
 
@@ -5,25 +7,34 @@ const https = require('https');
 
 const app = express();
 
-
+// set up get request to weather API on the load of the home page
 app.get("/", (req, res) => {
 
+    // url variable that holds the url for the weather Api
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Houston&appid=50dd39c51714469ae2ed1cf1fde3c6a1&units=imperial";
 
+    // using https get method
     https.get(url, function (response) {
+        // logging the response code
         console.log(response.statusCode);
 
         response.on("data", function (data) {
+            // logging data unparsed
             console.log(data);
 
+            // parsing the data and storing it in the weatherData variable
             const weatherData = JSON.parse(data);
 
+            // variable to hold temp
             const temp = weatherData.main.temp;
 
+            // variable to hold feels like temp
             const feelsLikeTemp = weatherData.main.feels_like;
 
+            // variable to hold weather description
             const weatherDescription = weatherData.weather[0].description;
 
+            // printing weatherData, temp, feelsLikeTemp and weather description below
             console.log(weatherData);
 
             console.log(temp);
@@ -41,7 +52,7 @@ app.get("/", (req, res) => {
 
 
 
-
+// setting up the server to listen on port 3000
 app.listen(3000, function () {
     console.log('listening on port 3000');
 })

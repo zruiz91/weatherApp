fc28455973095a1c4be888cea5b18ec38fbc8530
Zach Ruiz <powersbooth10@gmail.com>
2023-04-28 13:27:51 -0500

    added code to get a response back from weather api that fromats the data into JSON.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const https = require('https');
+
+
 
 const app = express();
 
 
 app.get("/", (req, res) => {
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Houston&appid=50dd39c51714469ae2ed1cf1fde3c6a1&units=imperial";
+
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            console.log(data);
+
+            const weatherData = JSON.parse(data);
+
+            console.log(weatherData);
+        })
+    })
     res.send("server is up and running");
 })
 
@@ -13,6 +30,6 @@ app.get("/", (req, res) => {
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('listening on port 3000');
 })
